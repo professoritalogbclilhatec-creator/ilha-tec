27a2fc575751d2235564a3fb523376326c2fdfd7
professoritalogbclilhatec-creator <professoritalogbclilhatec@gmail.com>
2026-05-23 15:10:52 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const C = 'ilhatec-v5';
+const ASSETS = ['/', '/index.html', '/icon-192.png', '/icon-512.png', '/manifest.json'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(C).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(
+    keys.filter(k => k !== C).map(k => caches.delete(k))
+  )));
+});
+self.addEventListener('fetch', e => e.respondWith(
+  caches.match(e.request).then(r => r || fetch(e.request))
+));
